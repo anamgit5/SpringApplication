943bf6ff2a65eea217203ada9b4731e418bcd776
Ana Mrkonjic <ana.mrkonjic@fer.hr>
2020-11-03 08:50:12 +0100

    first commit

---
+++ src/main/java/hr/fer/ilj/group1/SpringApplication/Application.java
@@ -0,0 +1,13 @@
+package hr.fer.ilj.group1.SpringApplication;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
